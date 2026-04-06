fc33a1ae8fbae67f15b246031d2839ffaec30677
kmiyazaki-pixel <k_miyazaki@vital-area.com>
2026-04-06 15:27:54 +0900

    Update page.tsx

--- frontend/src/app/calendar/week/page.tsx
+++ frontend/src/app/calendar/week/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import SchedulerShell from '@/components/SchedulerShell';
-
 export default function CalendarWeekPage() {
-  return (
-    <SchedulerShell title="週表示">
-      <div>週カレンダー本体</div>
-    </SchedulerShell>
-  );
+  return <div>週カレンダー本体</div>;
 }
