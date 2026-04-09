20ac6b47c0a8d110bad461c4a300d4be4ba6a8d1
kmiyazaki-pixel <k_miyazaki@vital-area.com>
2026-04-09 16:48:01 +0900

    Update CalendarRepository.java

--- backend/src/main/java/com/vitalarea/scheduler/repository/CalendarRepository.java
+++ backend/src/main/java/com/vitalarea/scheduler/repository/CalendarRepository.java
@@ -4,4 +4,6 @@ import com.vitalarea.scheduler.entity.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+
+    void deleteByOwnerUserId(Long ownerUserId);
 }
