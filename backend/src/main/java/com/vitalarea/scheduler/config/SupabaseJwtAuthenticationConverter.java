f32e65056fde4992ce61378b76befda90f30a77b
kmiyazaki-pixel <k_miyazaki@vital-area.com>
2026-04-21 10:35:07 +0900

    Create SupabaseJwtAuthenticationConverter.java

---
+++ backend/src/main/java/com/vitalarea/scheduler/config/SupabaseJwtAuthenticationConverter.java
@@ -0,0 +1,19 @@
+package com.vitalarea.scheduler.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+public class SupabaseJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        return new JwtAuthenticationToken(jwt, authorities, jwt.getSubject());
+    }
+}
